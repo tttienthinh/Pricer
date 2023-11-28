a1a1258b26f47cf0ca6ca8be3dff7f4fd77bfd45
tttienthinh <tranthuongtienthinh@gmail.com>
2023-11-28 23:33:24 +0100

    Premier jet Monte Carlo, les tests ne donne pas exactement la même chose sur les sites en ligne

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream> // cout
+#include <string.h> 
+#include <cstring> 
+#include <random>
+#include <typeinfo> // typeid().name
+#include <algorithm> // max
+using namespace std;
+
+
+float generate_normal_random(float mu, float sig) {
+    random_device rd; // Change le seed (racine) à chaque appel
+    default_random_engine generator(rd()); // Positionne le seed
+    normal_distribution<float> distribution(mu, sig); // Crée la distribution
+    return distribution(generator);
+}
+
+float get_price_at_T(float r, float vol, float S, int T) {
+    /*
+    On utilise la récursivité pour calculer le prix à T
+    */
+    if (T==0) {
+        return S;
+    } else {
+        float dW = generate_normal_random(0, 1);
+        float dS = S * (r + vol * dW);
+        return get_price_at_T(r, vol, (S+dS), (T-1));
+    }
+}
+
+float get_average(float* liste, int N) {
+    float avg = 0;
+    for (int i=0; i<N; i++) {
+        avg = avg + *(liste+i)/N;
+    }
+    return avg;
+}
+
+float simulation_Monte_Carlo(float r, float vol, float K, float S, int T, int N) {
+    float liste[N];
+    float* p=liste;
+    const float zero = 0;
+
+    for (int i=0; i<N; i++) {
+        float S_T = get_price_at_T(r, vol, S, T);
+        *(p+i) = max(S_T-K, zero);
+    }
+    return get_average(liste, N);
+}
+
+
+
+
+int main(){
+    float r = 0.02; // taux d'intérêt san risque
+    float vol = 0.05; // volatilité du prix de l'action
+    float K = 50; // Strike
+    float S = 40; // Valeur actuelle
+    int T = 10; // Maturité
+    int N = 100000;
+
+    
+    cout << simulation_Monte_Carlo(r, vol, K, S, T, N) << "\n";
+}
